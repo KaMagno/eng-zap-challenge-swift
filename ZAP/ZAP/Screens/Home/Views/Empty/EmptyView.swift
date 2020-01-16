0b20c88eca0b65219ae62fd54cfbfa9c87b704c8
Kaique Magno <kaique.magno.santos@accenture.com>
2020-01-16 00:21:06 -0300

    Prepare project to use frameworks at test target. Prepare test of EmptyView

--- ZAP/ZAP/Screens/Home/Views/Empty/EmptyView.swift
+++ ZAP/ZAP/Screens/Home/Views/Empty/EmptyView.swift
@@ -26,25 +26,44 @@ class EmptyView: UIView {
     }
 
     // MARK: - Functions
+    func setup(text: String) {
+        titleLabel.text = text
+    }
 }
 
 extension EmptyView: CodeView {
     func setupViewHierarchy() {
-        stackView.addArrangedSubview(titleLabel)
         stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(titleLabel)
         addSubview(stackView)
     }
+
     func setupConstraints() {
         //I did not use SnapKit in this view with porpose of show my knowledge
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 54).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -54).isActive = true
         stackView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         stackView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
 
+        let imageAspectRatio: CGFloat = 246 / 140
+        imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: imageAspectRatio).isActive = true
+        imageView.widthAnchor.constraint(lessThanOrEqualToConstant: 246).isActive = true
     }
+
     func setupAdditionalConfiguration() {
+        backgroundColor = .white
+
         stackView.axis = .vertical
         stackView.spacing = 8
 
-        imageView.image = UIImage(imageLiteralResourceName: "EmptyView")
+        imageView.image = UIImage(imageLiteralResourceName: "EmptyImage")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+
+        titleLabel.numberOfLines = 2
+        titleLabel.font = UIFont.systemFont(ofSize: 22)
+        titleLabel.textAlignment = .center
+
     }
 }
--- ZAP/ZAPTests/EmptyViewSpec.swift
+++ ZAP/ZAPTests/EmptyViewSpec.swift
@@ -8,3 +8,26 @@
 
 import Quick
 import Nimble
+import Nimble_Snapshots
+@testable
+import ZAP
+
+final class EmptyViewSpec: QuickSpec {
+    override func spec() {
+        var sut: EmptyView!
+
+        describe("EmptyView"){
+            beforeEach {
+                sut = EmptyView(frame: self.defaultFrame)
+            }
+            context("When initialize") {
+                beforeEach {
+                    sut.setup(text: "Nenhum imóvel disponivel \n no momento")
+                }
+                it("Must have the layout") {
+                    expect(sut) == snapshot()
+                }
+            }
+        }
+    }
+}
